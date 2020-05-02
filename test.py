3321ae9e83d4bbe999f8d3096f7156a9ff58493f
zhuuuuuuuuu <55119998+zhuuuuuuuuu@users.noreply.github.com>
2020-05-02 21:53:12 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,73 @@
+from sklearn.ensemble import RandomForestClassifier
+import math
+
+
+def Count_numbers(string):
+    num=0
+    for character in string:
+        if(character>'0' and character<='20'):
+            num+=1
+    return num
+
+def Calculate_entropy(string):
+    Map=Counter(string)
+    entropy=0.0
+    for val in Map.values():
+        entropy-=val/len(string)*math.log(val/len(string))
+    return entropy
+
+clf = RandomForestClassifier(random_state=0)
+domainlist = []
+class Domain:
+    def __init__(self,string,label):
+        self.length=len(string)
+        self.numbers=Count_numbers(string)
+        self.entropy=Calculate_entropy(string)
+        self.label=label
+    
+    def Return_data(self):
+        return [self.length,self.numbers,self.entropy]
+    
+    def Return_label(self):
+        if(self.label=="dga"):
+            return 0
+        else:
+            return 1
+
+def initData(filename):
+    with open(filename) as f:
+        lines=f.readlines()
+        for line in lines:
+            line=line.strip()
+            if line.startswith("#") or line =="":
+                continue
+            tokens=line.split(",")
+            domainlist.append(Domain(tokens[0],tokens[1]))
+
+def predictData(testfile,outputfile):
+    with open(testfile) as f1:
+        with open(outputfile,"w") as f2:
+            lines=f1.readlines()
+            for line in lines:
+                line=line.strip()
+                if line.startswith("#") or line =="":
+                    continue
+                P=clf.predict([[len(line),Count_numbers(line),Calculate_entropy(line)]])
+                if(P==[0]):
+                    f2.write(line+",dga\n")
+                else:
+                    f2.write(line+",notdga\n")
+
+def main():
+    initData("train.txt")
+    featureMatrix=[]
+    labelList=[]
+    for item in domainlist:
+	    featureMatrix.append(item.Return_data())
+	    labelList.append(item.Return_label())
+        
+    clf.fit(featureMatrix,labelList)
+    predictData("test.txt","result.txt")
+    
+if __name__=='__main__':
+    main()
